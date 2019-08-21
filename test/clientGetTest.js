bde4caaa0fe66394246f22d1624340bd3b6c6aae
Roshan Ghale <ghaleroshan@gmail.com>
2019-08-21 20:09:57 +1000

    Test for Get Multiple Clients
    - [x] Added mocha, nyc and loggerStub for test
    - [x] Tests for Get clients from db

---
+++ test/clientGetTest.js
@@ -0,0 +1,63 @@
+const assert = require("assert");
+const proxyquire = require("proxyquire").noCallThru();
+const loggerStub = require("./stubs/loggerStub");
+
+const mysqlStub = {};
+
+const clients = proxyquire("./../src/clients", {
+    "namshi-node-mysql": _ => mysqlStub,
+    "./logger": loggerStub
+});
+
+describe("clients", () => {
+    describe("getMultiple", () => {
+        it("should return no clients from the db when no clients exists", async () => {
+            try {
+                mysqlStub.query = (query, params) => {
+                    if (query.startsWith(`SELECT id, full_name`)) {
+                        return [];
+                    }
+                };
+
+                const result = await clients.getMultiple({});
+                assert.deepStrictEqual(result, []);
+            } catch (err) {
+                console.log(`err`, err);
+                assert.strictEqual(err.message, "should never reach here");
+            }
+        });
+        it("should return clients from the db when clients exists", async () => {
+            try {
+                mysqlStub.query = (query, params) => {
+                    if (query.startsWith(`SELECT id, full_name`)) {
+                        return [
+                            {
+                                id: 0,
+                                full_name:"Alice Limbu",
+                                gender:1,
+                                age:28,
+                                address:"Kogarah",
+                                phone: "0123456789",
+                                email: "alis@gmail.com",
+                            }
+                        ];
+                    }
+                };
+
+                const result = await clients.getMultiple({});
+                assert.deepStrictEqual(result[0], {
+                    id: 0,
+                    full_name:"Alice Limbu",
+                    gender:1,
+                    age:28,
+                    address:"Kogarah",
+                    phone: "0123456789",
+                    email: "alis@gmail.com",
+                });
+            } catch (err) {
+                console.log(`err`, err);
+                assert.deepStrictEqual(err.message, "should never reach here");
+            }
+        });
+    });
+});
