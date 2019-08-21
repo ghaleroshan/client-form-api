dde315c8fb7497360088f51efc637475cc19390a
Roshan Ghale <ghaleroshan@gmail.com>
2019-08-21 19:28:09 +1000

    - [x] Connects MySQL db to the project - [x] Gets all the clients in the db

--- src/clients.js
+++ src/clients.js
@@ -1,128 +1,116 @@
 const config = require("./config");
 const _ = require("lodash");
+const db = require("namshi-node-mysql")(config.db);
 const Joi = require("joi");
 const { joiValidate, httpError } = require("./utils");
 
+
 /**
- * Get all clients
+ * Get all clientDetails
  *
  * @param {*} params
  * Example curl:
  * curl -i -H "Accept: application/json"
  * -H "Content-Type: application/json"
- * -X GET http://localhost:9090/api/clients
+ * -X GET http://localhost:9090/api/clientDetails
  */
+async function getMultiple() {
 
-async function getMultiple(page) {
-    let offset = (page - 1) * [config.itemsPerPage];
-
-    let clients = await db.query(
-        `SELECT c.id, c.first_name, c.last_name, c.phone, c.email, c.password from clients LIMIT ${offset}, ${config.itemsPerPage}`
+    let clientDetails = await db.query(
+        `SELECT id, full_name, gender, age, address, phone, email FROM clientDetails`
     );
-    if (!clients.length) {
+    if (!clientDetails.length) {
         return [];
     }
-    console.log(`Found ${clients.length} clients in the db`);
-    return ;
+    console.log(`Found ${clientDetails.length} clientDetails in the db`);
+    return clientDetails;
 }
 
 /**
- * Get single clients by id
+ * Get single clientDetails by id
  *
  * @param {*} params
  * Example curl:
  * curl -i -H "Accept: application/json"
  * -H "Content-Type: application/json"
- * -X GET http://localhost:9090/api/clients/2
+ * -X GET http://localhost:9090/api/clientDetails/2
  */
 
 async function get(id) {
     let client = await db.query(
-        `SELECT e.id, e.first_name, e.last_name, e.phone, e.email, e.password, er.role, er.description
-    FROM client e LEFT JOIN client_role er on e.fk_client_role = er.id where e.id = ?`,
+        `SELECT id, full_name, age, gender, address FROM clientDetails where e.id = ?`,
         [id]
     );
     if (!client.length) {
         return [];
     }
 
-    console.log(`Found ${client.length} clients in the db`);
+    console.log(`Found ${client.length} clientDetails in the db`);
     return client;
 }
 
 function getclientCreateValidateRules() {
     return {
-        first_name: Joi.string()
+        full_name: Joi.string()
             .min(2)
             .max(255)
             .required()
             .error(new Error("First name is required")),
-        middle_name: Joi.string()
-            .allow("")
-            .error(new Error("middle name can only be a string")),
-        last_name: Joi.string()
-            .min(2)
-            .max(255)
-            .required()
-            .error(new Error("Last name is required")),
         phone: Joi.number()
             .integer()
             .required()
             .error(new Error("Phone number is required")),
-        position: Joi.string()
-            .min(2)
+        age: Joi.number()
+            .integer()
+            .required()
+            .error(new Error("Age is required")),
+        gender: Joi.string()
+            .required()
+            .error(new Error("Gender is required")),
+        address: Joi.string()
+            .min(5)
             .max(250)
             .required()
-            .error(new Error("Valid position is required")),
+            .error(new Error("Address is required")),
         email: Joi.string()
             .email({ minDomainAtoms: 2 })
             .error(new Error("Valid email address is required")),
-        password: Joi.string()
-            .min(5)
-            .regex(/^[a-zA-Z0-9]{3,30}$/)
-            .error(new Error("Password should be at least 5 character")),
-        role_id: Joi.number()
-            .positive()
-            .required()
-            .error(new Error("Category id is required as an positive number"))
     };
 }
 
-/** Create clients
+/** Create clientDetails
  * Example curl:
- * curl -i -XPOST http://localhost:9090/api/clients \
+ * curl -i -XPOST http://localhost:9090/api/clientDetails \
  * -H 'content-type: application/json;charset=UTF-8' \
  * -H 'accept: application/json' \
  * --data-binary  '{"first_name": "Mark", "middle_name":"", "last_name": "Hamilton", "phone":"0411018726", "email":"markhamil@gmail.com", "password":"", "role_id":1 }'
  * @param client
  */
 
-async function create(client) {
+async function create(clientDetails) {
     joiValidate(
-        client,
+        clientDetails,
         Joi.object(getclientCreateValidateRules()).required()
     );
 
     try {
         let res = await db.query(
-            `INSERT INTO client (first_name, middle_name, last_name, phone, position, email, password, fk_client_role)
-      VALUES (?,?,?,?,?,?,?,?)`,
+            `INSERT INTO clientDetails (full_name, age, gender, address, phone, email)
+      VALUES (?,?,?,?,?,?)`,
             [
-                client.first_name,
-                client.middle_name,
-                client.last_name,
-                client.phone,
-                client.position,
-                client.email,
-                client.password,
-                client.role_id
+                clientDetails.full_name,
+                clientDetails.phone,
+                clientDetails.age,
+                clientDetails.email,
+                clientDetails.gender,
+                clientDetails.phone
             ]
         );
         console.log(
-            `Created client ${client.first_name} with id ${res.insertId}`
+            `Created client ${clientDetails.full_name} with id ${res.insertId}`
         );
         return {
-            message: `client ${client.first_name} with id ${
+            message: `client ${clientDetails.full_name} with id ${
                 res.insertId
             } created successfully`
         };
@@ -134,19 +122,19 @@ async function create(client) {
 }
 
 /**
- * Update clients
+ * Update clientDetails
  *
  *
  * sample curl:
- * curl -ik -XPUT http://localhost:9090/api/clients/2
+ * curl -ik -XPUT http://localhost:9090/api/clientDetails/2
  * -H 'content-type: application/json;charset=UTF-8' \
  * -H 'accept: application/json'
  * --data-binary  '{"first_name": "Mark", "middle_name" : " ", "last_name": "Hamilton", "phone": 04110718726, "position":"Manager", "email": "markhamil@gmail.com, "password":"mark123", "role_id":1 }'
  * @param client
  */
 
-async function update(id, client) {
-    client = Object.assign(id, client);
+async function update(id, clientDetails) {
+    client = Object.assign(id, clientDetails);
     const validationRules = Object.assign(
         {
             id: Joi.number()
@@ -162,26 +150,24 @@ async function update(id, client) {
     joiValidate(client, Joi.object(validationRules).required());
     try {
         let res = await db.query(
-            `UPDATE client SET first_name = ?, middle_name = ?, last_name = ?, phone = ?, position = ?, email = ?, fk_client_role = ? 
+            `UPDATE clientDetails SET full_name = ?, age = ?, gender = ?, phone = ?, address = ?, email = ?, phone = ? 
         WHERE id = ?`,
             [
-                client.first_name,
-                client.middle_name,
-                client.last_name,
-                client.phone,
-                client.position,
-                client.email,
-                client.role_id,
-                client.id
-            ]
+                clientDetails.full_name,
+                clientDetails.phone,
+                clientDetails.age,
+                clientDetails.email,
+                clientDetails.gender,
+                clientDetails.phone
+            ],  clientDetails.id
         );
         console.log(
-            `Updated ${res.affectedRows} row(s) for client with id ${client.id}`
+            `Updated ${res.affectedRows} row(s) for client with id ${clientDetails.id}`
         );
 
         return {
-            message: `client ${client.first_name} with id ${
-                client.id
+            message: `client ${clientDetails.first_name} with id ${
+                clientDetails.id
             } updated successfully`
         };
     } catch (err) {
@@ -192,14 +178,14 @@ async function update(id, client) {
 }
 
 /**
- * Delete multiple clients
+ * Delete multiple clientDetails
  *
  * 'delete' is javascript reserved word so 'remove' is used as function name.
  *
  * @param {*} ids
  *
  * sample curl:
- * curl -ik -XPOST http://localhost:9090/api/clients/ \
+ * curl -ik -XPOST http://localhost:9090/api/clientDetails/ \
  * -H 'content-type: application/json;charset=UTF-8' \
  * -H 'accept: application/json'
  * --data-binary  '{"clientIds": [6,10,14]}'
--- src/config.js
+++ src/config.js
@@ -1,6 +1,14 @@
 const env = process.env;
 
 const config = {
+    db: {
+        host: env.DB_HOST || "sql12.freesqldatabase.com",
+        user: env.DB_USER || "sql12302553",
+        password: env.DB_PASSWORD || "EtPDwIJJ2Z",
+        database: env.DB_NAME || `sql12302553`
+    },
     env: process.env.NODE_ENV || "dev",
     itemsPerPage: 10
 };
+
+module.exports = config;
