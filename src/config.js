a661213fef114c12012ad465979dad10354fb83f
Roshan Ghale <ghaleroshan@gmail.com>
2019-09-30 18:41:11 +1000

    changed db

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const env = process.env;
 
 const config = {
-    db: {
-        host: env.DB_HOST || "sql12.freesqldatabase.com",
-        user: env.DB_USER || "sql12302553",
-        password: env.DB_PASSWORD || "EtPDwIJJ2Z",
-        database: env.DB_NAME || `sql12302553`
-    },
-    env: process.env.NODE_ENV || "dev",
-    itemsPerPage: 10
+  db: {
+    host: env.DB_HOST || "remotemysql.com",
+    user: env.DB_USER || "3d1EYk5tec",
+    password: env.DB_PASSWORD || "EtPDwIJJ2Z",
+    database: env.DB_NAME || `usyma4q2LJ`
+  },
+  env: process.env.NODE_ENV || "dev",
+  itemsPerPage: 10
 };
 
 module.exports = config;
