d59b2c33e95a9c8fe5d291e76af13b23d01ffca4
Roshan Ghale <ghaleroshan@gmail.com>
2019-09-30 18:54:20 +1000

    changed db

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ const config = {
   db: {
     host: env.DB_HOST || "remotemysql.com",
     user: env.DB_USER || "3d1EYk5tec",
-    password: env.DB_PASSWORD || "EtPDwIJJ2Z",
-    database: env.DB_NAME || `usyma4q2LJ`
+    password: env.DB_PASSWORD || "usyma4q2LJ",
+    database: env.DB_NAME || `3d1EYk5tec`
   },
   env: process.env.NODE_ENV || "dev",
   itemsPerPage: 10
